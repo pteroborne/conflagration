8610fff33f1cbf26a20cc7699ac5243c8e1e011c
Will <commanderwill2142@gmail.com>
2023-05-02 14:39:52 -0600

    commenting out fight logic for now

--- src/routes/conflict/[id]/fight/[id]/+page.svelte
+++ src/routes/conflict/[id]/fight/[id]/+page.svelte
@@ -1,59 +1,59 @@
-<script>
-    import {supabase} from "$lib/supabaseClient.js";
+<!--<script>-->
+<!--    import {supabase} from "$lib/supabaseClient.js";-->
 
-    export let userEngagement;
-    export let userCharacterId;
+<!--    export let userEngagement;-->
+<!--    export let userCharacterId;-->
 
-    let isPlayer1;
-    let isPlayer2;
+<!--    let isPlayer1;-->
+<!--    let isPlayer2;-->
 
-    $: {
-        isPlayer1 = userEngagement.player1_character_id === userCharacterId;
-        isPlayer2 = userEngagement.player2_character_id === userCharacterId;
-    }
+<!--    $: {-->
+<!--        isPlayer1 = userEngagement.player1_character_id === userCharacterId;-->
+<!--        isPlayer2 = userEngagement.player2_character_id === userCharacterId;-->
+<!--    }-->
 
-    // Subscribe function
-    export async function subscribe(tableName, filters, onUpdate) {
-        return supabase
-            .channel('custom-filter-channel')
-            .on(
-                'postgres_changes',
-                filters,
-                (payload) => {
-                    console.log('Received Payload', payload);
-                    onUpdate(payload.new, payload.eventType);
-                }
-            )
-            .subscribe();
-    }
+<!--    // Subscribe function-->
+<!--    export async function subscribe(tableName, filters, onUpdate) {-->
+<!--        return supabase-->
+<!--            .channel('custom-filter-channel')-->
+<!--            .on(-->
+<!--                'postgres_changes',-->
+<!--                filters,-->
+<!--                (payload) => {-->
+<!--                    console.log('Received Payload', payload);-->
+<!--                    onUpdate(payload.new, payload.eventType);-->
+<!--                }-->
+<!--            )-->
+<!--            .subscribe();-->
+<!--    }-->
 
-    // Handle updates
-    const handleUpdate = (newData, eventType) => {
-        const {player1_roll, player2_roll} = newData;
+<!--    // Handle updates-->
+<!--    const handleUpdate = (newData, eventType) => {-->
+<!--        const {player1_roll, player2_roll} = newData;-->
 
-        if (player1_roll !== null && player2_roll !== null) {
-            // Perform comparison
-            const winner = player1_roll > player2_roll ? 'Player 1' : 'Player 2';
+<!--        if (player1_roll !== null && player2_roll !== null) {-->
+<!--            // Perform comparison-->
+<!--            const winner = player1_roll > player2_roll ? 'Player 1' : 'Player 2';-->
 
-            // Reset rolls
-            supabase
-                .from('engagements')
-                .update({player1_roll: null, player2_roll: null})
-                .match({ /* match conditions */});
+<!--            // Reset rolls-->
+<!--            supabase-->
+<!--                .from('engagements')-->
+<!--                .update({player1_roll: null, player2_roll: null})-->
+<!--                .match({ /* match conditions */});-->
 
-            console.log(`\${winner} wins!`);
-        }
-    };
+<!--            console.log(`\${winner} wins!`);-->
+<!--        }-->
+<!--    };-->
 
-    // Subscribe to the engagements table
-    const unsubscribe = subscribe('engagements', { /* filters */}, handleUpdate);
+<!--    // Subscribe to the engagements table-->
+<!--    const unsubscribe = subscribe('engagements', { /* filters */}, handleUpdate);-->
 
-    // Update player's roll
-    const {data, error} = await supabase
-        .from('engagements')
-        .update({player1_roll: diceRollValue})
-        .match({player1_character_id: characterId});
+<!--    // Update player's roll-->
+<!--    const {data, error} = await supabase-->
+<!--        .from('engagements')-->
+<!--        .update({player1_roll: diceRollValue})-->
+<!--        .match({player1_character_id: characterId});-->
 
-</script>
+<!--</script>-->
 <h1 class="title is-1">You've made it to the fight</h1>
 
